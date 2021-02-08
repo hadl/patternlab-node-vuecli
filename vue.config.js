9cdd6bf6c632580e96c3cf3618d3664a2054d82a
Hadl <hadl@users.noreply.github.com>
2021-02-08 11:36:40 +0100

    fix: patternlab vue plugin config used in production build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
+const { getIfUtils } = require('webpack-config-utils');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const {
   patternlabVuePluginConfig
 } = require('./buildScripts/patternlabWebpackPlugins');
+const { ifProduction } = getIfUtils(process.env.NODE_ENV || '');
 
 // vue.config.js
 module.exports = {
@@ -16,7 +18,9 @@ module.exports = {
   },
 
   chainWebpack: (config) => {
-    patternlabVuePluginConfig(config);
+    if (!ifProduction()) {
+      patternlabVuePluginConfig(config);
+    }
     config.plugin('stylelint-webpack-plugin').use(new StyleLintPlugin({
       files: ['**/*.{vue,scss}'],
     }));
