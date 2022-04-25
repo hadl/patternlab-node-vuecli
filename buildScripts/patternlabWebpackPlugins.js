0ddf090e28a7ab0ffe1bf01d3132d4473fd4241f
Hadl <hadl@users.noreply.github.com>
2022-04-25 17:13:35 +0200

    feat: migrate vue-cli v4 -> v5 and using VueJS V2

--- buildScripts/patternlabWebpackPlugins.js
+++ buildScripts/patternlabWebpackPlugins.js
@@ -1,26 +1,44 @@
+/* eslint-disable no-param-reassign */
+const { join, resolve } = require('path');
 const PatternLabPlugin = require('./PatternlabPlugin');
 
-const { getIfUtils } = require('webpack-config-utils');
-const { join } = require('path');
-const VueService = process.VUE_CLI_SERVICE;
-
 const plConfig = require('../patternlab-config.json');
-const { ifDevelopment } = getIfUtils(process.env.NODE_ENV);
-const contentBaseDir = join(VueService.context, plConfig.paths.public.root);
 
+const ifDevelopment = process.env.NODE_ENV !== 'production';
+const contentBaseDir = resolve('./', plConfig.paths.public.root);
+
+/**
+ * update and add plugins
+ * @param config
+ */
 const patternlabVuePluginConfig = (config) => {
   config
     .plugin('html')
-    .tap(args => {
+    .tap((args) => {
       args[0].template = join(contentBaseDir, 'index.html');
       return args;
     });
 
-  if (ifDevelopment()) {
-    config.plugin('patternlabplugin').use(PatternLabPlugin)
+  if (ifDevelopment) {
+    config.plugin('patternlabplugin')
+      .use(PatternLabPlugin)
+      .before('html');
   }
+};
 
-  config.devServer.contentBase(contentBaseDir);
+/**
+ * Change dev server directory in configureWebpack
+ * @param config
+ */
+const patternlabVueWebpackConfig = (config) => {
+  if (!config.devServer) {
+    config.devServer = {};
+  }
+  if (!config.devServer.static) {
+    config.devServer.static = {};
+  }
+  config.devServer.static.directory = contentBaseDir;
 };
 
 exports.patternlabVuePluginConfig = patternlabVuePluginConfig;
+exports.patternlabVueWebpackConfig = patternlabVueWebpackConfig;
--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
 
 import './scss/global-styles.scss';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   render: (h) => h(App),
 }).$mount('#app');
