d52ae234fa809c442c86727973c842b3964b4151
Hadl <hadl@users.noreply.github.com>
2022-04-28 20:00:28 +0200

    feat: add @fullhuman/postcss-purgecss

---
+++ postcss.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const purgecss = require('@fullhuman/postcss-purgecss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    autoprefixer,
+    purgecss({
+      content: ['./public/**/*.html', './src/**/*.vue', './source/**/*.twig'],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(
+          /<style[^]+?<\/style>/gi,
+          '',
+        );
+        return (
+          contentWithoutStyleBlocks.match(
+            /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g,
+          ) || []
+        );
+      },
+      safelist: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/,
+      ],
+    }),
+  ],
+};
